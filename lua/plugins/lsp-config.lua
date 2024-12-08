c34cf6a13345a0eaf0cfcd9b9c078994c5f8657e
Elio Molinaro <emolinaro.work@gmail.com>
2024-12-08 22:31:22 +0100

    fix(lsp-config): change lsp to basedpyright

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,7 +18,13 @@ return {
 
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.jedi_language_server.setup({ capabilities = capabilities })
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities,
+                settings = {
+                    basedpyright = {
+                        typeCheckingMode = "off" }
+                }
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
