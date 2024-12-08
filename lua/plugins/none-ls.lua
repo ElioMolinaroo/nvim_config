5b591688524c40274be0e653a1425e89d6e07b5e
Elio Molinaro <emolinaro.work@gmail.com>
2024-12-08 21:00:05 +0100

    fix(none-ls): add length arg to black formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,11 +5,11 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black.with({extra_args={"--line-length=250"}}),
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.black,
             },
         })
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
     end,
 }
