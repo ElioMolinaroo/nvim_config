850fe6d0dc844b4f3a2ce4c5c77f2b06289eae87
Elio Molinaro <emolinaro.work@gmail.com>
2024-12-30 21:41:54 +0100

    feat(obsidian): add obsidian support for nvim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,6 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set conceallevel=1")
 vim.cmd("filetype plugin indent on")
 vim.g.mapleader = " "
